3a491a29f220f6c9b92c1fef2fc22cbe61c1fbba
JaeMinNa <149379194+JaeMinNa@users.noreply.github.com>
2024-03-21 05:04:22 +0900

    [Fix] SpawnSystem
    - SpawnSystem 스테이지별로 몬스터 이름, 스폰 시간 입력 기능 추가

--- Assets/03.Scripts/SpawnSystem/SpawnSystem.cs
+++ Assets/03.Scripts/SpawnSystem/SpawnSystem.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class SpawnSystem : MonoBehaviour
 {
+    [System.Serializable]
+    public struct StageInfo
+    {
+        public int Stage;
+        public string[] enemys; // enemy + 생성되는 시간 입력 ex) "Snail 5"
+    }
+    public List<StageInfo> Stages;
+
+    private int _currentStage;
+    private string _enemy;
+    private int _spawnTime;
+
     [SerializeField] private Transform _spawnLeft;
     [SerializeField] private Transform _spawnRigth;
 
     private void Start()
     {
-        StartCoroutine(COSpawnEnemy());
+        _currentStage = GameManager.I.DataManager.GameDataSO.Stage;
+        for (int i = 0; i < Stages[_currentStage - 1].enemys.Length; i++)
+        {
+            string[] words = Stages[_currentStage - 1].enemys[i].Split(' ');
+            _enemy = words[0];
+            _spawnTime = int.Parse(words[1]);
+
+            StartCoroutine(COSpawnEnemy(_enemy, _spawnTime));
+        }
     }
 
-    IEnumerator COSpawnEnemy()
+    IEnumerator COSpawnEnemy(string enemy, int time)
     {
         while (true)
         {
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(time);
 
             int random = Random.Range(0, 2);
-            if (random == 0) GameManager.I.ObjectPoolManager.InstantiateEnemy("Snail", _spawnLeft.position);
-            else GameManager.I.ObjectPoolManager.InstantiateEnemy("Snail", _spawnRigth.position);
+            if (random == 0) GameManager.I.ObjectPoolManager.InstantiateEnemy(enemy, _spawnLeft.position);
+            else GameManager.I.ObjectPoolManager.InstantiateEnemy(enemy, _spawnRigth.position);
         }
     }
 }
